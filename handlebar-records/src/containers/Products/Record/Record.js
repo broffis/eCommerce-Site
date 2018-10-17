e6dbd58f73301564c41ad51d8313934ff0673ad3
Brandon <broffis@redventures.com>
2018-10-17 17:34:52 -0400

    Updated CSS for records/carosel player/text overlay

--- handlebar-records/src/containers/Products/Record/Record.js
+++ handlebar-records/src/containers/Products/Record/Record.js
@@ -8,7 +8,7 @@ const record = (props) => (
             <img src={props.albumCover} alt='${props.albumName} by ${props.artist}' />
             <figcaption>{props.albumName} <br/> by {props.artist} <br/> Price: ${props.price}</figcaption>
         </figure>
-        <iframe src={props.spotifyURI} width="auto" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src={props.spotifyURI} className={classes.Player} width="auto" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         {/* <p>{props.albumName}</p>
         <p>Artist: {props.artist}</p>
         <p>Price: ${props.price}.00</p> */}
